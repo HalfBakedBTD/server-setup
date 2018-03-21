e5b256b0c65059c783e38b08af32b6b1618a15d3
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-21 13:50:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,36 @@ bot.on("message", async message => {
 	if (message.content === 's!invite') {
 		return message.channel.send(`<@${message.author.id}>, here is a link to add me to your server:\n:link: https://discordapp.com/api/oauth2/authorize?client_id=426009292429066241&permissions=8&scope=bot :link:`)
 	}
- });
+	if (message.content === 's!basic-setup') {
+		if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("`Invalid server permissions to use this command.`");
+		message.channel.send("`Building basic text channels.`")
+		message.guild.createChannel('welcome', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.guild.createChannel('chat', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.guild.createChannel('bots', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.guild.createChannel('pictures', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.guild.createChannel('memes', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.guild.createChannel('spam', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.guild.createChannel('suggestions', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.guild.createChannel('support', 'text')
+      .then(console.log)
+      .catch(console.error);
+		message.channel.send("`Finished building basic text channels.`")
+		return message.author.send("`I have just build basic text channels in your server!`\n\n```You now need to edit catagories and possibly user permissions until you get it how you like it! Try to personalize your server a bit so it will be unique!")
+	}
+});
 
 bot.login(process.env.BOT_TOKEN);
